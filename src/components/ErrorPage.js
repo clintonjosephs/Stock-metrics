142e282d300b9a7e41b7856f48332722e22a2048
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-09 15:00:25 +0100

    fixed routing error

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import {
   Container, Row,
 } from 'react-bootstrap';
 import styles from './styles/Dashboard.module.css';
 import errorImg from '../images/nonetwork.png';
 
-const ErrorPage = () => (
-  <>
-    <section className="pb-5">
-      <Container className={styles.marginCreate} onClick={() => { window.location.reload(); }}>
-        <Row
-          className={`${styles.row} justify-content-center align-content-center mx-lg-5 mt-5`}
-          style={{ background: 'white', borderRadius: '20px', cursor: 'pointer' }}
-        >
-          <img src={errorImg} alt="network error" />
-          {' '}
-          <h3 className="text-center">Tap to refresh</h3>
-        </Row>
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <section className="pb-5">
+        <Container className={styles.marginCreate} onClick={() => { navigate('/'); window.location.reload(); }}>
+          <Row
+            className={`${styles.row} justify-content-center align-content-center mx-lg-5 mt-5`}
+            style={{ background: 'white', borderRadius: '20px', cursor: 'pointer' }}
+          >
+            <img src={errorImg} alt="network error" />
+            {' '}
+            <h3 className="text-center">Tap to refresh</h3>
+          </Row>
 
-      </Container>
-    </section>
-  </>
-);
+        </Container>
+      </section>
+    </>
+  );
+};
 
 export default ErrorPage;
